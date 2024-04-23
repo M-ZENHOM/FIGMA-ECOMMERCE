29923508893c0decc387e70f6b2e371043dc82b8
ZENHOM <zenhom.dev@gmail.com>
2024-04-23 19:35:18 +0200

    chore: import sections on page/layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "@/components/layouts/Header";
+import { inter } from "@/lib/fonts";
+import Navbar from "@/components/layouts/Navbar";
+import Footer from "@/components/layouts/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
