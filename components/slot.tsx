ff77c9a86257c44dcf85e919087bd6f67c390290
ZENHOM <zenhom.dev@gmail.com>
2024-04-23 19:31:15 +0200

    feat: add slot component

---
+++ components/slot.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+export type AsChildProps<DefaultElementProps> =
+  | ({ asChild?: false } & DefaultElementProps)
+  | { asChild: true; children: React.ReactNode };
+export function Slot({
+  children,
+  ...props
+}: React.HTMLAttributes<HTMLElement> & {
+  children?: React.ReactNode;
+}) {
+  if (React.isValidElement(children)) {
+    return React.cloneElement(children, {
+      ...props,
+      ...children.props,
+      style: {
+        ...props.style,
+        ...children.props.style,
+      },
+      className: cn(props.className, children.props.className),
+    });
+  }
+  if (React.Children.count(children) > 1) {
+    React.Children.only(null);
+  }
+  return null;
+}
